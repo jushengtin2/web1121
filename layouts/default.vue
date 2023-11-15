f467dd140d1e99a49c842d9f5dc92ebf7aa82bd7
beans-liu <144888087+beans-liu@users.noreply.github.com>
2023-11-15 08:31:02 +0000

    Update 4 files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div id="top">
-      <NuxtPage></NuxtPage>
       <button @click="toggleMenu">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +16,7 @@
         </svg>
       </button>
     </div>
-
+    <NuxtPage></NuxtPage>
     <div :class="{ sidebar: true, active: menuVisible }">
       <br />
       <div id="appname">APP NAME</div>
@@ -134,7 +133,18 @@
         <NuxtLink to="/contact" class="nuxt-link-no-underline">創作</NuxtLink>
       </div>
     </div>
-    <BooksPicture />
+    <div class="block">
+      <h3>推薦</h3>
+      <div class="books">
+        <div class="books-container" ref="booksContainer">
+          <BooksPicture :book="books[currentIndex].value" />
+        </div>
+      </div>
+      <div class="booksbtn">
+        <button class="prev-button" @click="prevBook">上一本</button>
+        <button class="next-button" @click="nextBook">下一本</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -145,7 +155,46 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from "vue";
 import BooksPicture from "~/components/BooksPicture.vue";
+
+const sampleBook1 = ref({
+  title: "Book Title",
+  imageUrl:
+    "https://www.friendlyseed.com.tw/sites/default/files/field_opinion_image/feng_mian_.jpg", // Replace with a valid image URL
+  description: "Book description goes here",
+});
+const sampleBook2 = ref({
+  title: "Book",
+  imageUrl: "https://gpi.culture.tw/pic/news/9953.jpg", // Replace with a valid image URL
+  description: "description goes here",
+});
+const books = [sampleBook1, sampleBook2]; // Array containing book references
+
 const menuVisible = ref(false);
+const booksContainer = ref(null); // 參考 books-container 元素
+const currentIndex = ref(0); // 當前顯示的書籍索引
+
+// 切換到上一本書
+function prevBook() {
+  if (currentIndex.value > 0) {
+    currentIndex.value--;
+    scrollToCurrentBook();
+  }
+}
+// 切換到下一本書
+function nextBook() {
+  if (currentIndex.value < books.length - 1) {
+    currentIndex.value++;
+    scrollToCurrentBook();
+  }
+}
+
+// 滾動到當前書籍的位置
+function scrollToCurrentBook() {
+  const container = booksContainer.value;
+  const bookWidth = container.offsetWidth; // 每本書的寬度
+  const scrollX = currentIndex.value * bookWidth; // 計算滾動位置
+  container.scrollTo({ left: scrollX, behavior: "smooth" });
+}
 
 function toggleMenu(event) {
   // 阻止事件冒泡，避免立即触发 handleClickOutside
